7b058657a06f4bff27b5b83b752588003a4c3699
saynotrish <trishaamv.sayno@gmail.com>
2023-07-11 15:36:55 +0800

    deleting stuff

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector(".toggle-menu");
-const headNav = document.querySelector(".header__nav");
-const branding = document.querySelector(".branding")
-
-toggle.addEventListener("click", () => {
-    headNav.classList.toggle("open");
-    branding.classList.toggle("open");
-    toggle.classList.toggle("open");v
-})
